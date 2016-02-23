8dbb6276e536d0ac85b1db4dec914001c5382503
Pawneet Singh <pawneet.dev@gmail.com>
2016-02-24 02:19:04 +0530

    Delete added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,12 @@ class ProductsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to :action => 'index'
+	end
+
 	private
 		def product_params
 			params.require(:product).permit(:name, :price)
