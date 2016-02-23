f25cb1ca42d56e47949c2dcd7cf86e4962753586
Pawneet Singh <pawneet.dev@gmail.com>
2016-02-24 02:08:48 +0530

    Create, read and update

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+	
+	def new
+
+	end
+
+	def create
+		@product =  Product.new(product_params)
+		if @product.save
+			redirect_to action: 'index'
+		else
+			render :action => 'new'
+		end
+	end
+
+	def show
+		@article = Product.find(params[:id])
+	end
+
+	def index
+		@products = Product.all
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update(product_params)
+			redirect_to :action => "index"
+		else
+			render 'edit'
+		end
+	end
+
+	private
+		def product_params
+			params.require(:product).permit(:name, :price)
+		end
+end
